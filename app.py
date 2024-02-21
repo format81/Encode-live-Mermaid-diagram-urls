6de1d41533b8f115bbc8e24c398b43a2e07f74ff
Antonio Formato <44245400+format81@users.noreply.github.com>
2024-02-21 10:53:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,23 @@ def genPakoLink(graphMarkdown: str):
     link = 'http://mermaid.live/edit#pako:' + dEncode.decode('ascii')
     return link
 
-# User Interface Creation
-st.title("Mermaid Code Renderer")
+# Sidebar content
+st.sidebar.title("Project Info")
+st.sidebar.write("This is a simple Streamlit app for rendering Mermaid diagrams.")
+st.sidebar.write("Contact: antonio.formato@gmail.com")
+st.sidebar.write("[GitHub Repository](https://github.com/format81/Encode-live-Mermaid-diagram-urls)")
+st.sidebar.write("[Mermaid.js](https://mermaid.js.org/intro/)")
+st.sidebar.write("[TI Mindmap - GitHub](https://github.com/format81/TI-Mindmap-GPT)")
+st.sidebar.write("[TI Mindmap - Streamlit App](https://ti-mindmap-gpt.streamlit.app/)")
+
+st.markdown(
+        "If you find this small project useful, follow me on [LinkedIn]https://www.linkedin.com/in/antonioformato/ and give me a ⭐ Star on GitHub: [![Star on GitHub](https://img.shields.io/github/stars/format81/Encode-live-Mermaid-diagram-urls?style=social)](https://github.com/format81/Encode-live-Mermaid-diagram-urls)"
+    )."
+    )
+
+
+# Main interface
+st.title("Mermaid.live Code Renderer")
 
 # Mermaid code input
 mermaid_code = st.text_area("Enter Mermaid code here")
